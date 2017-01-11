b6f3833a1539f2059ea12d7766f6b0c8f6dbe754
Sebastian Morales <sebastian.moralesd@gmail.com>
2017-01-10 21:52:47 -0800

    Add new menu item to create a base level scene

---
+++ Assets/Tools/LevelCreator/Editor/EditorUtils.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+using System.Linq;
+
+namespace RunAndJump.LevelCreator
+{
+    public static class EditorUtils
+    {
+        public static void NewScene()
+        {
+            //EditorApplication.SaveCurrentSceneIfUserWantsTo();
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+            //EditorApplication.NewScene();
+            EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects);
+        }
+
+        public static void CleanScene()
+        {
+            var allObjects = Object.FindObjectsOfType<GameObject>().ToList();
+            allObjects.ForEach(Object.DestroyImmediate);
+        }
+
+        public static void NewLevel()
+        {
+            NewScene();
+            CleanScene();
+            var levelGameObject = new GameObject("Level");
+            levelGameObject.transform.position = Vector3.zero;
+            levelGameObject.AddComponent<Level>();
+        }
+    }
+}
---
+++ Assets/Tools/LevelCreator/Editor/MenuItems.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace RunAndJump.LevelCreator {
+
+    public static class MenuItems
+    {
+        [MenuItem ("Tools/Level Creator/New Level Scene")]
+        public static void NewLevel()
+        {
+            EditorUtils.NewLevel();
+        }
+    }
+}
