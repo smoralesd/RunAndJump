4a5c52a14e5dc5d21d2ffcbd66738d04769ce3be
Sebastian Morales <sebastian.moralesd@gmail.com>
2017-01-16 22:06:12 -0800

    Add coordinate conversion functions (grid <-> world)

--- Assets/Scripts/Level/Level.cs
+++ Assets/Scripts/Level/Level.cs
@@ -74,7 +74,6 @@ namespace RunAndJump
             Gizmos.color = oldColor;
         }
 
-
         private void DrawGridGizmo(int rows, int columns)
         {
             DrawVerticalGridLines(rows, columns);
@@ -104,6 +103,37 @@ namespace RunAndJump
         private static Vector3 GetGridLinePoint(int forRow, int forColumn)
         {
             return new Vector3(forColumn * GridCellSize, forRow * GridCellSize, 0);
+        }
+
+        public Vector3 WorldToGridCoordinates(Vector3 point)
+        {
+            var xCoordinate = (int) ((point.x - transform.position.x) / GridCellSize);
+            var yCoordinate = (int) ((point.y - transform.position.y) / GridCellSize);
+
+            return new Vector3(xCoordinate, yCoordinate, 0.0f);
+        }
+
+        public Vector3 GridToWorldCoordinates(int column, int row)
+        {
+            var xCoordinate = transform.position.x + (column * GridCellSize + GridCellSize / 2.0f);
+            var yCoordinate = transform.position.y + (row * GridCellSize + GridCellSize / 2.0f);
+
+            return new Vector3(xCoordinate, yCoordinate, 0.0f);
+        }
+
+        public bool IsInsideGridBounds(Vector3 point)
+        {
+            float minX = transform.position.x;
+            float maxX = minX + _totalColumns * GridCellSize;
+            float minY = transform.position.y;
+            float maxY = minY + _totalRows * GridCellSize;
+
+            return (minX <= point.x && point.x <= maxX) && (minY <= point.y && point.y <= maxY);
+        }
+
+        public bool IsInsideGridBounds(int col, int row)
+        {
+            return (0 <= col && col < _totalColumns) && (0 <= row && row < _totalRows);
         }
 
     }
