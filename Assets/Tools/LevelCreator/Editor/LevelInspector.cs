1580e8de63c0952fa7eade3d75bd622b13656197
Sebastian Morales <sebastian.moralesd@gmail.com>
2017-01-21 14:18:30 -0800

    Add dummy level inspector

---
+++ Assets/Tools/LevelCreator/Editor/LevelInspector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace RunAndJump.LevelCreator
+{
+    [CustomEditor(typeof(Level))]
+    public class LevelInspector: Editor
+    {
+        private Level _myTarget;
+
+        private void OnEnable()
+        {
+            _myTarget = (Level)target;
+        }
+
+        private void OnDisable()
+        {
+        }
+
+        private void OnDestroy()
+        {
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.LabelField("The GUI of this inspector was modified.");
+            EditorGUILayout.LabelField("The current level time is: " + _myTarget.TotalTime);
+        }
+    }
+}
