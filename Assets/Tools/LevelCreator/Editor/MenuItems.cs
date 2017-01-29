c4e47876bc4aad727166f8dbfc87feab2b208c17
Sebastian Morales <sebastian.moralesd@gmail.com>
2017-01-28 16:26:38 -0800

    Add basic, empty Palette Window. Show it from our Tools menu item

--- Assets/Tools/LevelCreator/Editor/MenuItems.cs
+++ Assets/Tools/LevelCreator/Editor/MenuItems.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 using UnityEditor;
 
-namespace RunAndJump.LevelCreator {
+namespace RunAndJump.LevelCreator
+{
 
     public static class MenuItems
     {
-        [MenuItem ("Tools/Level Creator/New Level Scene")]
+        [MenuItem("Tools/Level Creator/New Level Scene")]
         public static void NewLevel()
         {
             EditorUtils.NewLevel();
+        }
+
+        [MenuItem("Tools/Level Creator/Show Palette")]
+        public static void ShowPalette()
+        {
+            PaletteWindow.ShowPalette();
         }
     }
 }
---
+++ Assets/Tools/LevelCreator/Editor/PaletteWindow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace RunAndJump.LevelCreator
+{
+    public class PaletteWindow : EditorWindow
+    {
+        public static PaletteWindow instance;
+
+        public static void ShowPalette()
+        {
+            instance = GetWindow<PaletteWindow>();
+            instance.titleContent = new GUIContent("Palette");
+        }
+    }
+}
